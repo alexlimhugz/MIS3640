b00affb2d592b272fa54fdf08e083769337f1862
alexlimhugz <31518696+alexlimhugz@users.noreply.github.com>
2017-09-26 13:55:05 -0400

    first quiz uploaded

---
+++ session07/iteration_demo.py
@@ -0,0 +1,30 @@
+# result = 0
+
+# for i in range (10):
+#     result = result + i + 1
+#     print('in the {}th iteration, result = {}'
+#     .format(i, result))
+
+# print(result)
+
+
+
+
+# result = 1
+
+# for i in range (10):
+#     result = result * (1 + i)
+#     print('in the {}th iteration, result = {}'
+#     .format(i, result))
+
+# print(result)
+
+
+# result=0
+# i n range(1, 1000, 2):
+#     result = result + i
+    
+# print(result)
+
+for i in 'hello':
+    print(i)
